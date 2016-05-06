22d4da908a0c31b634658788861af7a108e3702f
hyg <934533623@qq.com>
2016-05-06 10:01:38 +0800

    Initial Commit

---
+++ UICollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionView
+//
+//  Created by 侯英格 on 16/5/6.
+//  Copyright © 2016年 侯英格. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UICollectionView
+//
+//  Created by 侯英格 on 16/5/6.
+//  Copyright © 2016年 侯英格. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
